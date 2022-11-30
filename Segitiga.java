c13fa4aa29ec4ea8fd24b0a895e90fdec6f74695
Notio21 <95495311+Notio21@users.noreply.github.com>
2022-11-30 13:26:09 +0700

    Menambahkan file Segitiga.java

---
+++ Segitiga.java
@@ -0,0 +1,21 @@
+public class Segitiga extends BangunDatar{
+    int alas;
+    int tinggi;
+
+    public Segitiga(String warna, int alas, int tinggi){
+        super(warna);
+        this.alas = alas;
+        this.tinggi = tinggi;
+    }
+
+    @Override
+    public void gambar(){
+        System.out.println("Drawing Segitiga");
+
+    }
+
+    @Override
+    public float luas(){
+        return alas * tinggi * 1/2;
+    }
+}
